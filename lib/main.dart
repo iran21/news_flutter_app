f25d1eb08fe590f8673a6374e39bed02f9444f8c
iran21 <iransneto2@gmail.com>
2021-03-23 20:28:32 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'News app'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  String title;
+
+  MyHomePage({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Container(),
+    );
+  }
+}
---
+++ lib/settings.dart
@@ -0,0 +1,3 @@
+class Settings {
+  static String apiKey = 'f4928ec4-4c0d-4a90-b9b0-e57dfd7a8477';
+}
